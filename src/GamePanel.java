53cc1aa8809fd1bd4c6a94df89aec6a365f8a2ff
zotthy <se05110800@gmail.com>
2024-07-01 20:35:37 +0200

    move and shoot

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,36 +2,112 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class GamePanel extends JPanel {
     private Tank tank1;
+    private Tank tank2;
+    private List<Bullet> bullets;
+    private Set<Integer> pressedKeys;
 
     public GamePanel() {
         setFocusable(true);
-        setBackground(Color.BLACK);
-
         tank1 = new Tank(100, 300);
+        tank2 = new Tank(500, 300);
+        bullets = new ArrayList<>();
+        pressedKeys = new HashSet<>();
 
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                switch (e.getKeyCode()) {
-                    case KeyEvent.VK_UP -> tank1.move(0, -5);
-                    case KeyEvent.VK_DOWN -> tank1.move(0, 5);
-                    case KeyEvent.VK_LEFT -> tank1.move(-5, 0);
-                    case KeyEvent.VK_RIGHT -> tank1.move(5, 0);
-                }
-                repaint();
+                pressedKeys.add(e.getKeyCode());
+                handleKeyPress(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                pressedKeys.remove(e.getKeyCode());
             }
         });
 
-        Timer timer = new Timer(1000 / 60, e -> repaint());
+        Timer timer = new Timer(1000 / 60, e -> {
+            handlePressedKeys();
+            bullets.forEach(Bullet::update);
+            repaint();
+        });
         timer.start();
+    }
+
+    private void handleKeyPress(int keyCode) {
+        int targetAngle;
+        switch (keyCode) {
+            case KeyEvent.VK_SPACE:
+                int correctedAngle = (tank1.getAngle() + 90) % 360;
+                bullets.add(new Bullet(tank1.getX() + 45, tank1.getY() + 45, correctedAngle));
+                break;
+            case KeyEvent.VK_SHIFT:
+                int correctedAngle2 = (tank2.getAngle() + 90) % 360;
+                bullets.add(new Bullet(tank2.getX() + 45, tank2.getY() + 45, correctedAngle2));
+                break;
+        }
+    }
+
+    private void handlePressedKeys() {
+        for (int keyCode : pressedKeys) {
+            int targetAngle;
+            switch (keyCode) {
+                case KeyEvent.VK_UP:
+                    targetAngle = 0;
+                    tank1.move(0, -5,TankWars.x, TankWars.y, targetAngle - tank1.getAngle());
+                    break;
+                case KeyEvent.VK_DOWN:
+                    targetAngle = 180;
+                    tank1.move(0, 5, TankWars.x, TankWars.y, targetAngle - tank1.getAngle());
+                    break;
+                case KeyEvent.VK_LEFT:
+                    targetAngle = 90;
+                    tank1.move(-5, 0, TankWars.x, TankWars.y, targetAngle - tank1.getAngle());
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    targetAngle = 270;
+                    tank1.move(5, 0, TankWars.x, TankWars.y, targetAngle - tank1.getAngle());
+                    break;
+                case KeyEvent.VK_W:
+                    targetAngle = 0;
+                    tank2.move(0, -5, TankWars.x, TankWars.y, targetAngle - tank2.getAngle());
+                    break;
+                case KeyEvent.VK_S:
+                    targetAngle = 180;
+                    tank2.move(0, 5, TankWars.x, TankWars.y, targetAngle - tank2.getAngle());
+                    break;
+                case KeyEvent.VK_A:
+                    targetAngle = 90;
+                    tank2.move(-5, 0, TankWars.x, TankWars.y, targetAngle - tank2.getAngle());
+                    break;
+                case KeyEvent.VK_D:
+                    targetAngle = 270;
+                    tank2.move(5, 0, TankWars.x, TankWars.y, targetAngle - tank2.getAngle());
+                    break;
+            }
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         tank1.draw(g);
+        tank2.draw(g);
+        bullets.forEach(bullet -> bullet.draw(g));
     }
 }
--- src/Tank.java
+++ src/Tank.java
@@ -19,31 +19,51 @@ public class Tank {
         this.y = y;
         this.angle = 0;
         try {
-            tankImage = ImageIO.read(new File("src/Assets/tankmodel.jpg"));
-        }catch (IOException e){
+            tankImage = ImageIO.read(new File("src/Assets/Tank.png"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
         double rotationRequired = Math.toRadians(-angle);
-        double locationX = tankImage.getWidth() / 2;
-        double locationY = tankImage.getHeight() / 2;
+        double locationX = tankImage.getWidth() / 6;
+        double locationY = tankImage.getHeight() / 6;
         AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
         AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
 
-        g2d.drawImage(op.filter(tankImage, null), x - SIZE / 2, y - SIZE / 2, null);
+        // Scale image
+        double scaleFactor = 0.3;
+        BufferedImage scaledTankImage = new BufferedImage((int) (tankImage.getWidth() * scaleFactor), (int) (tankImage.getHeight() * scaleFactor), tankImage.getType());
+        Graphics2D g2dScaled = scaledTankImage.createGraphics();
+        g2dScaled.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2dScaled.drawImage(tankImage, 0, 0, scaledTankImage.getWidth(), scaledTankImage.getHeight(), null);
+        g2dScaled.dispose();
+
+        g2d.drawImage(op.filter(scaledTankImage, null), x - SIZE / 4, y - SIZE / 4, null);
 
     }
-    public void move(int dx, int dy) {
-        x += dx;
-        y += dy;
+
+    public void move(int dx, int dy, int panelWidth, int panelHeight, int r) {
+
+        // Sprawdzanie pozycji czołgu
+        int NX = x + dx;
+        int NY = y + dy;
+
+        if (NX - SIZE / 2 >= 0 && NX + SIZE / 2 <= panelWidth && NY - SIZE / 2 >= 0 && NY + SIZE / 2 <= panelHeight) {
+            x = NX;
+            y = NY;
+        }
+
+        this.angle += r;
+
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
+        System.out.println("r:" + angle);
     }
 
-    public void rotate(int dAngle) {
-        angle += dAngle;
-    }
 
     public int getX() {
         return x;
